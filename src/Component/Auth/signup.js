1436430f4f4c90e1088f3e3c3f80ab855188ec56
Critchkn <mtgelliott15@gmail.com>
2020-04-21 16:53:07 -0500

    Change Sign Up button text

--- src/Component/Auth/signup.js
+++ src/Component/Auth/signup.js
@@ -38,7 +38,7 @@ class signUp extends Component {
             <input type="text" id="lastName" onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
           </div>
         </form>
       </div>
