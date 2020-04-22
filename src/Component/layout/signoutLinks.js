e972daeae3341c2cc701b32f35ca5dd7d050250f
Critchkn <mtgelliott15@gmail.com>
2020-04-22 14:56:19 -0500

    Set up basic redux store

--- src/Component/layout/signoutLinks.js
+++ src/Component/layout/signoutLinks.js
@@ -5,10 +5,10 @@ const SignOutLinks = () => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/">Sign Up</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log In</NavLink>
+        <NavLink to="/signin">Log In</NavLink>
       </li>
     </ul>
   );
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    { id: 1, title: "Lorem Ipsum", content: "dolar set blah" },
+    { id: 2, title: "Lorem Ipsum", content: "dolar set blah" },
+    { id: 3, title: "Lorem Ipsum", content: "dolar set blah" },
+  ],
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
